c6be38137eba2bd7e5d850b84aff30f441ff9665
Jöran Tesse <jte@iteratec.de>
2017-11-22 17:40:46 +0100

    added price times amount for most recent trade

--- api/httpdocs/main.js
+++ api/httpdocs/main.js
@@ -13,18 +13,22 @@ elem ({
 					children: (() => {
 						var ret = [];
 						[
-							' ',
+							'&nbsp;',
 							'last request',
 							'last trade price',
 							'last trade amount',
+							'price &times; amount',
 							'ask price',
 							'ask amount',
+							'price &times; amount',
 							'bid price',
-							'bid amount'
-						].forEach (spalte => {
+							'bid amount',
+							'price &times; amount'
+						].forEach ((spalte, n) => {
 							ret.push (elem ({
 								tag: 'th',
-								innerText: spalte
+								className: 'column-' + n,
+								innerHTML: spalte
 							}));
 						});
 						return ret;
@@ -46,10 +50,12 @@ function Currency (name) {
 		children: [
 			elem ({
 				tag: 'td',
+				className: 'column-0',
 				innerText: name
 			}),
 			timr = elem ({
 				tag: 'td',
+				className: 'column-1',
 				innerText: '0'
 			})
 		],
@@ -61,29 +67,38 @@ function Currency (name) {
 		timr.innerText = delta.join ('.') + ' seconds';
 		if (JSON.stringify (values) === currentValues) return;
 		currentValues = JSON.stringify (values);
+		values.lastTradeVolume = eur (values.lastTradePrice * values.lastTradeAmount);
+		values.askVolume = eur (values.askPrice * values.askAmount);
+		values.bidVolume = eur (values.bidPrice * values.bidAmount);
 		[
 			'lastTradePrice',
 			'lastTradeAmount',
+			'lastTradeVolume',
 			'askPrice',
 			'askAmount',
+			'askVolume',
 			'bidPrice',
-			'bidAmount'
-		].forEach (spalte => {
-			if (!elems[spalte]) elems[spalte] = elem ({ tag: 'td', appendTo: croot });
+			'bidAmount',
+			'bidVolume'
+		].forEach ((spalte, n) => {
+			if (!elems[spalte]) elems[spalte] = elem ({ tag: 'td', className: 'column-' + (n+2), appendTo: croot });
 			elems[spalte].innerText = values[spalte];
 		});
 	};
 }
+
+var SORTED_CURRENCIES = ['XBT', 'BCH', 'DASH', 'ZEC', 'LTC', 'ETC', 'XRP'];
 
 function refresh () {
 	ajax.get ('last-values', (err, result) => {
 		if (err) console.log (err);
 		else {
 			SERVER_TIME = result.serverTime;
-			for (var curr in result.values) {
+			SORTED_CURRENCIES.forEach (curr => {
+				if (!result.values[curr]) return;
 				if (!currencies[curr]) currencies[curr] = new Currency (curr);
 				currencies[curr].update (result.values[curr]);
-			}
+			});
 		}
 		window.requestAnimationFrame (() => {
 			timer = setTimeout (refresh, 300 + Math.random () * 400 | 0);
@@ -91,3 +106,13 @@ function refresh () {
 	});
 }
 refresh ();
+
+
+function eur (n) {
+	var sn = ('' + (Math.round (n * 100) / 100)).split ('.');
+	if (sn.length === 1) sn.push ('00');
+	else {
+		while (sn[1].length < 2) sn[1] += '0';
+	}
+	return sn.join ('.') + '€';
+}
